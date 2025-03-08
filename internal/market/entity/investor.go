a125d9d55a969501a9b417b37195e4af19dd0f3d
Alexsander <alexsander1996@yahoo.com.br>
2025-03-08 12:53:46 -0300

    conclusao do assetposition e do investor
    - Concluída a estrutura base para manuseio das posições dos ativos de um investidor;

--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -13,7 +13,41 @@ func NewInvestor(id string) *Investor {
 	}
 }
 
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPostion = append(i.AssetPostion, assetPosition)
+}
+
+func (i *Investor) AdjustAssetPosition(assetID string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+
+	if assetPosition == nil {
+		i.AssetPostion = append(i.AssetPostion, NewInvestorAssetPosition(assetID, qtdShares))
+	} else {
+		assetPosition.AddShares(qtdShares)
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPostion {
+		if assetPosition.AssetID == assetID {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
 type InvestorAssetPosition struct {
 	AssetID string
 	Shares  int
+}
+
+func NewInvestorAssetPosition(assetID string, qtdShares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  qtdShares,
+	}
+}
+
+func (iap *InvestorAssetPosition) AddShares(qtd int) {
+	iap.Shares += qtd
 }
